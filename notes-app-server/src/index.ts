df700011f33dcc08940fc66c97792b3e2c27b711
Theodor Johansson <t.j.u@telia.com>
2024-03-01 12:20:17 +0100

    populate ui with notes from db

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -40,6 +40,12 @@ app.put("/api/notes/:id", async (req, res) => {
     const {title, content} = req.body
     const id = parseInt(req.params.id)
 
+    if (!title || !content) {
+        return res
+            .status(400)
+            .send("title and content fields required")
+    }
+
     if (!id || isNaN(id)) {
         return res
             .status(400)
@@ -52,6 +58,27 @@ app.put("/api/notes/:id", async (req, res) => {
             data: {title, content}
         })
         res.json(updatedNote)
+    } catch (error) {
+        res
+            .status(500)
+            .send("Oops, something went wrong")
+    }
+})
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = parseInt(req.params.id)
+
+    if (!id || isNaN(id)) {
+        return res
+            .status(400)
+            .send("ID must be a valid integer")
+    }
+
+    try {
+        await prisma.note.delete({
+            where: {id}
+        })
+        res.status(204).send()
     } catch (error) {
         res
             .status(500)
